2642eb1c19cff67b894cc9b8bc03cbeb7703cc05
Daniel Lim <hbt@markhamboards.com>
2016-08-01 14:23:43 -0400

    init infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+      path: __dirname,
+      filename: 'app/js/main.js'
+    },
+    module: [
+      {
+        test: /\jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  };
+};
