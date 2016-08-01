bee8e1318efdc85d2293766d9a34135ef46beaae
Daniel Lim <hbt@markhamboards.com>
2016-08-01 16:10:04 -0400

    Updating
    Updating

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
 // alert('It Works!');
-ReactDOM.render(
-  <App />, 
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
