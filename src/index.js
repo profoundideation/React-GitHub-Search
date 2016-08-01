e389ef94060fe1b8856e89fec6ef8a0a0d71b9d0
Daniel Lim <hbt@markhamboards.com>
2016-08-01 15:52:20 -0400

    update src
    update src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('It Works!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+// alert('It Works!');
+ReactDOM.render(
+  <App />, document.getElementById('app')
+);
