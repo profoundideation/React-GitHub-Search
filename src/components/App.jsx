09ddb236cd0b21e74be4f0c3af722206ef960002
Daniel Lim <hbt@markhamboards.com>
2016-08-02 19:51:14 -0400

    Part2 Completed
    Part 2 Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,34 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'profoundhub',
+      userProfile: [],
+      userData: [],
+      userRepos: [],
+      perPage: 10
+    }
+  }
+
   render() {
     return (
       <div>
-        Now you see me ...
+        {this.state.userName}
       </div>
     )
   }
+}
+
+App.propTypes = {
+  clientId: React.PropTypes.string,
+  clientSecret: React.PropTypes.string,
+};
+
+App.defaultProps = {
+  client: '0a59ddc213a95ee9adb6',
+  clientSecret: '655a3af9c98d47e2e7e5c1b6aeed2ad696104c0a'
 }
 
 export default App
